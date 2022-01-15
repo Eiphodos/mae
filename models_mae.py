e0dc784378528f710db2b8c1ed7fe75878003c93
Eiphodos <david.hagerman.olzon@gmail.com>
2022-01-15 14:57:27 +0100

    removed a line that really should not be there.

--- models_mae.py
+++ models_mae.py
@@ -33,8 +33,6 @@ class MaskedAutoencoderViT(nn.Module):
         self.patch_embed = PatchEmbed(img_size, patch_size, in_chans, embed_dim)
         num_patches = self.patch_embed.num_patches
 
-        self.in_chans
-
         self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
         self.pos_embed = nn.Parameter(torch.zeros(1, num_patches + 1, embed_dim), requires_grad=False)  # fixed sin-cos embedding
 
