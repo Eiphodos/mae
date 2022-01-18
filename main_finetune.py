76b8f5c4360aecc83e71492a0ed7f8e46f4bbe27
Eiphodos <david.hagerman.olzon@gmail.com>
2022-01-18 11:20:03 +0100

    Changed to tensorboardX summarywriter

--- main_finetune.py
+++ main_finetune.py
@@ -19,7 +19,7 @@ from pathlib import Path
 
 import torch
 import torch.backends.cudnn as cudnn
-from torch.utils.tensorboard import SummaryWriter
+from tensorboardX import SummaryWriter
 
 import timm
 
@@ -195,7 +195,7 @@ def main(args):
 
     if global_rank == 0 and args.log_dir is not None and not args.eval:
         os.makedirs(args.log_dir, exist_ok=True)
-        log_writer = SummaryWriter(log_dir=args.log_dir)
+        log_writer = SummaryWriter(logdir=args.log_dir)
     else:
         log_writer = None
 
