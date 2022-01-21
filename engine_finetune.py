309bfaaaa99e12512b46f1c559781f3b65ce7fee
david.hagerman.olzon <david.hagerman.olzon@gmail.com>
2022-01-21 15:42:36 +0100

    Wrong argmentname for log dir when using tensorboardX

--- engine_finetune.py
+++ engine_finetune.py
@@ -38,7 +38,7 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
     optimizer.zero_grad()
 
     if log_writer is not None:
-        print('log_dir: {}'.format(log_writer.log_dir))
+        print('log_dir: {}'.format(log_writer.logdir))
 
     for data_iter_step, (samples, targets) in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
 
--- engine_pretrain.py
+++ engine_pretrain.py
@@ -34,7 +34,7 @@ def train_one_epoch(model: torch.nn.Module,
     optimizer.zero_grad()
 
     if log_writer is not None:
-        print('log_dir: {}'.format(log_writer.log_dir))
+        print('log_dir: {}'.format(log_writer.logdir))
 
     for data_iter_step, (samples, _) in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
 
