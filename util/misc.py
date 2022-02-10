82dd5199ea58c0ae1cd5659f7607840e009e7e4a
Eiphodos <david.hagerman.olzon@gmail.com>
2022-02-10 14:10:52 +0100

    Added extra logging info in regards to SLURM

--- util/misc.py
+++ util/misc.py
@@ -242,6 +242,10 @@ def init_distributed_mode(args):
     args.dist_backend = 'nccl'
     print('| distributed init (rank {}): {}, gpu {}'.format(
         args.rank, args.dist_url, args.gpu), flush=True)
+    if 'SLURM_PROCID' in os.environ:
+        srank = int(os.environ['SLURM_PROCID'])
+        devc = torch.cuda.device_count()
+        print("Slurm rank is: {} and device count is: {}".format(srank, devc))
     torch.distributed.init_process_group(backend=args.dist_backend, init_method=args.dist_url,
                                          world_size=args.world_size, rank=args.rank)
     torch.distributed.barrier()
